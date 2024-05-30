b54a518915246b6529e184269999d90a233e0baf
MthsHelm <1038581@hr.nl>
2024-05-30 16:05:37 +0200

    Update t_camera.py

--- t_camera.py
+++ t_camera.py
@@ -10,6 +10,7 @@ url = "http://145.24.238.10:8080//shot.jpg"
 # While loop to continuously fetching data from the Url
 while True:
 	img_resp = requests.get(url)
+	print("succus")
 	img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
 	img = cv2.imdecode(img_arr, -1)
 	img = imutils.resize(img, width=1000, height=1800)
